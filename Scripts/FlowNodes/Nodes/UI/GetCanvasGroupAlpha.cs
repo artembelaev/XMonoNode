e6efbaac39cb50924ef47fe24f4a989c93a812c6
AleksandrDavletbaev-ural <davletbaev.a@ural-games.ru>
2022-04-25 21:30:50 +0500

    add canvas group.alpha get/set

---
+++ Scripts/FlowNodes/Nodes/UI/GetCanvasGroupAlpha.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+namespace XMonoNode
+{
+    [CreateNodeMenu("UI/Get CanvasGroup Alpha", 426)]
+    public class GetCanvasGroupAlpha : GetObjectParameter<CanvasGroup, float>
+    {
+        protected override float GetValue(CanvasGroup obj)
+        {
+#if UNITY_EDITOR
+            if(obj != null)
+            {
+#endif
+                return obj.alpha;
+
+#if UNITY_EDITOR
+            }
+            else
+            {
+                return 0;
+            }
+#endif
+        }
+    }
+}
---
+++ Scripts/FlowNodes/Nodes/UI/SetCanvasGroupAlpha.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+namespace XMonoNode
+{
+    [CreateNodeMenu("UI/Set CanvasGroup Alpha", 427)]
+    public class SetCanvasGroupAlpha : SetObjectParameter<CanvasGroup, float>
+    {
+        protected override void SetValue(CanvasGroup obj, float value)
+        {
+            obj.alpha = value;
+        }
+    }
+}
