e515490c0315fbd2e0d067113fdb7fcff1cfbb37
ArtemBelaev-ural <belyaev.a@ural-games.ru>
2022-08-25 16:03:20 +0500

    Animator nodes

---
+++ Scripts/FlowNodes/Nodes/Animation/Animator/AnimatorSetParam.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using XMonoNode;
+
+namespace XMonoNode
+{
+    public abstract class AnimatorSetParam : FlowNodeInOut 
+    {
+        [SerializeField, Hiding, HideLabel]
+        public Animator animator;
+
+        [SerializeField, Hiding, HideLabel]
+        public string paramName;
+
+
+        public override object GetValue(NodePort port) 
+        {
+            return null;
+        }
+    }
+
+    public abstract class AnimatorSetParam<T> : AnimatorSetParam
+    {
+        [Input(connectionType: ConnectionType.Override, typeConstraint: TypeConstraint.Inherited), HideLabel]
+        public T                                Value;
+
+
+        private NodePort                        ValuePort = null;
+        private int                             hash = -1;
+        protected override void Init()
+        {
+            base.Init();
+            ValuePort = GetInputPort(nameof(Value));
+        }
+
+        public override void Flow(NodePort flowPort)
+        {
+            T value = ValuePort.GetInputValue(Value);
+            SetAnimatorValue(animator, paramName, value);
+        }
+
+        protected abstract void SetAnimatorValue(Animator animator, string name, T value);
+    }
+}
---
+++ Scripts/FlowNodes/Nodes/Animation/Animator/AnimatorSetParamBool.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using XMonoNode;
+
+namespace XMonoNode
+{
+    [CreateNodeMenu("Animation/Animator/Bool", 127)]
+    public class AnimatorSetParamBool : AnimatorSetParam<bool> 
+    {
+        protected override void SetAnimatorValue(Animator animator, string name, bool value)
+        {
+            animator.SetBool(name, value);
+        }
+    }
+
+}
